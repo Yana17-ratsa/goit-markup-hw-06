e6c25fc4c764f99570139aaf8443857bcb8299a9
Yana17-ratsa <ratsayana13@gmail.com>
2024-09-30 13:54:57 +0300

    js edited

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const backdrop = document.querySelector('.backdrop');
 orderBtn.addEventListener('click', () => {
   backdrop.classList.add('is-open');
 });
+
+const closeModalBtn = document.querySelector('.modal-btn');
+
+if (closeModalBtn && backdrop) {
+  closeModalBtn.addEventListener('click', () => {
+    backdrop.classList.remove('is-open');
+  });
+}
